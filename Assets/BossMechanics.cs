b3c365446eed3da08a50a2264740c2ad7e2dca0d
T3MPGAMER <78913308+DanielThomasProctor@users.noreply.github.com>
2021-05-17 00:51:02 +0100

    Boss fight damage with flames
    the dragon now damages the player if they are in the zone that the dragon is in. some walls could be added to separate the arena for the user better.
    
    Next step is to create a blast radius for the enemy ai. this will depend on how close the player is to the ai to how much damage the dammed ai will do.

--- Assets/BossMechanics.cs
+++ Assets/BossMechanics.cs
@@ -23,12 +23,16 @@ public class BossMechanics : MonoBehaviour
     public GameObject DragonMiddleStatue2;//1
     public GameObject DragonMiddleFire;//1
     public int Dragonnumber = 1;
-
+    /// ///////////////////////////////////////////////////////
+    public GameObject trigger1;
+    public GameObject trigger2;
+    public GameObject trigger3;
     /// ///////////////////////////////////////////////////////
     public GameObject shotClass;
     public float speed;
     float DragonDelay;
     public float Delay;
+    FlameArena Flamesss;
     // Start is called before the first frame update
     void Start()
     {
@@ -50,16 +54,21 @@ public class BossMechanics : MonoBehaviour
         DragonMiddleStatue2.SetActive(false);
         DragonMiddle.SetActive(false);
         //Move = GameObject.FindGameObjectWithTag("BossFightTrigger").GetComponent<>();
-
+        trigger1.SetActive(false);
+        trigger2.SetActive(false);
+        trigger3.SetActive(false);
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Bossfight == true)
         {
             if (DragonDelay < Time.time)
             {
+              
                 DragonDelay = Delay + Time.time;
                 if (Dragonnumber == 1)
                 {
@@ -82,15 +91,22 @@ public class BossMechanics : MonoBehaviour
     }
     IEnumerator DragonLeft1(float DragonLeft11)
     {
+        trigger1.SetActive(false);
+        trigger2.SetActive(false);
+        trigger3.SetActive(false);
         yield return new WaitForSeconds(2);
         DragonLeftStatue1.SetActive(true);
         DragonLeftStatue2.SetActive(true);
         yield return new WaitForSeconds(4);
         DragonLeftFire.SetActive(true);
+        trigger1.SetActive(true);
+        Flamesss = GameObject.FindGameObjectWithTag("Triggers").GetComponent<FlameArena>();
         yield return new WaitForSeconds(4);
+        Flamesss.attackingFlames = false;
         DragonLeftFire.SetActive(false);
         DragonLeftStatue1.SetActive(false);
         DragonLeftStatue2.SetActive(false);
+        trigger1.SetActive(false);
         yield return new WaitForSeconds(10);
         DragonLeft.SetActive(false);
         Debug.Log("Time to switch");
@@ -98,15 +114,23 @@ public class BossMechanics : MonoBehaviour
     }
     IEnumerator DragonRight2(float DragonRight11)
     {
+        trigger1.SetActive(false);
+        trigger2.SetActive(false);
+        trigger3.SetActive(false);
         yield return new WaitForSeconds(2);
         DragonRightStatue1.SetActive(true);
         DragonRightStatue2.SetActive(true);
         yield return new WaitForSeconds(4);
+      
         DragonRightFire.SetActive(true);
+        trigger2.SetActive(true);
+        Flamesss = GameObject.FindGameObjectWithTag("Triggers").GetComponent<FlameArena>();
         yield return new WaitForSeconds(4);
+        Flamesss.attackingFlames = false;
         DragonRightFire.SetActive(false);
         DragonRightStatue1.SetActive(false);
         DragonRightStatue2.SetActive(false);
+        trigger2.SetActive(false);
         yield return new WaitForSeconds(10);
         DragonRight.SetActive(false);
         Debug.Log("Time to switch");
@@ -114,15 +138,23 @@ public class BossMechanics : MonoBehaviour
     }
     IEnumerator DragonMiddle3(float DragonMiddle11)
     {
+        trigger1.SetActive(false);
+        trigger2.SetActive(false);
+        trigger3.SetActive(false);
         yield return new WaitForSeconds(2);
         DragonMiddleStatue1.SetActive(true);
         DragonMiddleStatue2.SetActive(true);
         yield return new WaitForSeconds(4);
+
         DragonMiddleFire.SetActive(true);
+        trigger3.SetActive(true);
+        Flamesss = GameObject.FindGameObjectWithTag("Triggers").GetComponent<FlameArena>();
         yield return new WaitForSeconds(4);
+        Flamesss.attackingFlames = false;
         DragonMiddleFire.SetActive(false);
         DragonMiddleStatue1.SetActive(false);
         DragonMiddleStatue2.SetActive(false);
+        trigger3.SetActive(false);
         yield return new WaitForSeconds(10);
         DragonMiddle.SetActive(false);
         Debug.Log("Time to switch");
---
+++ Assets/FlameArena.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameArena : MonoBehaviour
+{
+    BossMechanics BossMec;
+    public float Delay;
+    public float fireDelay = 2f;
+    PlayerHealth Health1;
+    public bool attackingFlames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BossMec = GameObject.FindGameObjectWithTag("Mechanics").GetComponent<BossMechanics>();
+        Health1 = GameObject.FindGameObjectWithTag("Mechanics").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(attackingFlames == true)
+        {
+            if ( Time.time > Delay)
+            {
+                Delay = Time.time + fireDelay;
+                Health1.BarFill-= 5;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Flames") )
+        {
+            attackingFlames = true;
+         
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Flames"))
+        {
+            attackingFlames = false;
+
+        }
+    }
+}
