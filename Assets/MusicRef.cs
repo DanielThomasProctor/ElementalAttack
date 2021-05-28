9e078952d7daf3d0139c8b6e1788b7750b45453c
T3MPGAMER <78913308+DanielThomasProctor@users.noreply.github.com>
2021-05-28 10:41:59 +0100

    Missed music references

---
+++ Assets/MusicRef.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRef : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //PatrolSong found at https://youtu.be/nWUi28R1GMQ by Rise Again by Alexander Nakarada [ Cinematic / Piano / Epic ] | free-stock-music.com
+    //Boss fight song https://www.youtube.com/watch?v=9gBTKiVqprE by  Epic Battle Music (No Copyright) "Dragon Castle" by Makai Symphony 🇯🇵
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
